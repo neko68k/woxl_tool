cc1b9d18863ce79ddae7d4968dea4d8dd8ec6a5b
Shaun Thompson <neko68k@gmail.com>
2012-06-24 17:10:01 -0600

    fixed up 4-bit tim vram autoplace

--- woxl_tool/tim.cpp
+++ woxl_tool/tim.cpp
@@ -12,56 +12,76 @@ static WORD PalAutoX = 0;
 static WORD PalAutoY = 0;
 static WORD TimAutoX = 0;
 static WORD TimAutoY = 0;
-static BYTE HeightFlag = 0;
 
-void TIM_Load(BINFILE *intim){
-	CHK_HDR palHdr;
-	CHK_HDR pixHdr;
+
+void TIM_Load(BINFILE *intim, bool isLibrary){
+	CHK_HDR *palHdr;
+	CHK_HDR *pixHdr;
+
+	BYTE *TIM = intim->base;
 
 	BYTE *pix = NULL;
 	BYTE *pal = NULL;
 	
-	DWORD timHdr2 = 0;
+	DWORD *timHdr2 = (DWORD*)TIM+4;
 
 	BYTE mult = 2;
 
 	DWORD magic;
-	binread(&magic, 4, 1, intim);
+	//binread(&magic, 4, 1, intim);
 
-	binread(&timHdr2, 4, 1, intim);	
+	//binread(&timHdr2, 4, 1, intim);	
 
-	if(timHdr2 & 7 == 1){
+	if(*timHdr2 & 7 == 1){
 		//do 8-bit
 	}
 	else{
 		//do 4-bit
 		binread(&palHdr, sizeof(CHK_HDR), 1, intim);
+		//palHdr = (CHK_HDR*)TIM+
+		if(TIM+56==0&&TIM+58==0){		// if img x/y == 0
+			*(WORD*)(TIM+12)=PalAutoX;	// pal x
+			*(WORD*)(TIM+14)=PalAutoY;	// pal y
+			*(WORD*)(TIM+16)=0x10;		// pal w
+			*(WORD*)(TIM+18)=1;			// pal h
+			*(WORD*)(TIM+56)=TimAutoX;	// img x
+			*(WORD*)(TIM+58)=TimAutoY;	// img y
 
-		palHdr.x=PalAutoX;
-		palHdr.y=PalAutoY;
-		palHdr.w=0x10;
-		palHdr.h=1;
+			PalAutoX += 16;
 
-		PalAutoX += 0x10;
+			if(PalAutoX >= 384){
+				PalAutoX = 320;
+				++PalAutoY;
+			}
+			if(isLibrary)
+				TimAutoY += 32;
+			else
+				TimAutoY += 128;
 
-		if(PalAutoX == 0x180){
-			PalAutoX = 0x140;
-			PalAutoY++;
+			if(TimAutoY >= 256){
+				if(isLibrary)
+					TimAutoX += 8;
+				else
+					TimAutoX += 32;
+				TimAutoY = 0;
+			}
 		}
-
-		pal = (BYTE*)calloc(palHdr.w*palHdr.h, 2);
-		binread(pal, palHdr.w*palHdr.h, 2, intim);
 	}
 
-		if(timHdr2 == 9 || timHdr2 == 8){
-			
-		}
+		//pal = (BYTE*)calloc(palHdr.w*palHdr.h, 2);
+		//binread(pal, palHdr.w*palHdr.h, 2, intim);
 
-		binread(&pixHdr, sizeof(CHK_HDR), 1, intim);
-		pix = (BYTE*)calloc(pixHdr.w*pixHdr.h, mult);
-		binread(pix, pixHdr.w*pixHdr.h, mult, intim);
-	if(timHdr2 == 9 || timHdr2 == 8)
-		VRAM_LoadTex(pix, pal, pixHdr, pixHdr.w*pixHdr.h*mult, palHdr, palHdr.w*palHdr.h*2);
-	else
-		VRAM_LoadTex(pix, pixHdr, pixHdr.w*pixHdr.h*mult);
+		//binread(&pixHdr, sizeof(CHK_HDR), 1, intim);
+
+		//if(timHdr2 == 9 || timHdr2 == 8){
+			
+		//}
+
+		
+		//pix = (BYTE*)calloc(pixHdr.w*pixHdr.h, mult);
+		//binread(pix, pixHdr.w*pixHdr.h, mult, intim);
+	//if(timHdr2 == 9 || timHdr2 == 8)
+		//VRAM_LoadTex(pix, pal, pixHdr, pixHdr.w*pixHdr.h*mult, palHdr, palHdr.w*palHdr.h*2);
+	//else
+		//VRAM_LoadTex(pix, pixHdr, pixHdr.w*pixHdr.h*mult);
 }
